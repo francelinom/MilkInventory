27130b281532923853ce57ce6834f10a324168dd
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-03-13 11:19:54 -0300

    criando tela inicial

--- frontend/frontmilkinventary/src/layouts/MainLayout.vue
+++ frontend/frontmilkinventary/src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Milk Inventary
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div></div>
       </q-toolbar>
     </q-header>
 
@@ -30,7 +30,7 @@
           header
           class="text-grey-8"
         >
-          Essential Links
+          Menu
         </q-item-label>
         <EssentialLink
           v-for="link in essentialLinks"
@@ -61,40 +61,34 @@ export default {
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
+          title: 'Home',
+          caption: '',
+          icon: 'home',
+          link: '/'
         },
         {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
+          title: 'Estoque',
+          caption: '',
+          icon: 'view_agenda',
+          link: ''
         },
         {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
+          title: 'Relatórios',
+          caption: '',
+          icon: 'list_alt',
+          link: ''
         },
         {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
+          title: 'Cadastrar Item',
+          caption: '',
+          icon: 'library_add',
+          link: ''
         },
         {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
+          title: 'Sair',
+          caption: '',
+          icon: 'exit_to_app',
+          link: '/login'
         }
       ]
     }
